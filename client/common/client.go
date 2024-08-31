d64323e2faabcbe4092826fc8e8a84ff5657eeb4
Abraham Osco <aosco@fi.uba.ar>
2024-08-31 14:26:12 -0300

    now client, when the connection to server fails, terminates and don't make more attempts, and readme is udpated

--- client/common/client.go
+++ client/common/client.go
@@ -75,38 +75,6 @@ func (c *Client) CloseSocket() {
 	}
 }
 
-func (c *Client) StartClient(msgID int) {
-	// Create the connection the server in every loop iteration. Send an
-	err := c.createClientSocket()
-	if err != nil {
-		log.Criticalf("action: loop_finished | result: fail | client_id: %v", c.config.ID)
-		return
-	}
-
-	// TODO: Modify the send to avoid short-write
-	fmt.Fprintf(
-		c.conn,
-		"[CLIENT %v] Message N°%v\n",
-		c.config.ID,
-		msgID,
-	)
-	msg, err := bufio.NewReader(c.conn).ReadString('\n')
-	c.CloseSocket()
-	if err != nil {
-		log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
-			c.config.ID,
-			err,
-		)
-		return
-	}
-	log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
-		c.config.ID,
-		msg,
-	)
-	// Wait a time between sending one message and the next one
-	time.Sleep(c.config.LoopPeriod)
-}
-
 // StartClientLoop Send messages to the client until some time threshold is met
 func (c *Client) StartClientLoop() {
 	// There is an autoincremental msgID to identify every message sent
@@ -118,7 +86,34 @@ func (c *Client) StartClientLoop() {
 		if thereWasASignal {
 			break
 		}
-		c.StartClient(msgID)
+		err := c.createClientSocket()
+		if err != nil {
+			log.Criticalf("action: loop_finished | result: fail | client_id: %v", c.config.ID)
+			return
+		}
+
+		// TODO: Modify the send to avoid short-write
+		fmt.Fprintf(
+			c.conn,
+			"[CLIENT %v] Message N°%v\n",
+			c.config.ID,
+			msgID,
+		)
+		msg, err := bufio.NewReader(c.conn).ReadString('\n')
+		c.CloseSocket()
+		if err != nil {
+			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
+				c.config.ID,
+				err,
+			)
+			return
+		}
+		log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
+			c.config.ID,
+			msg,
+		)
+		// Wait a time between sending one message and the next one
+		time.Sleep(c.config.LoopPeriod)
 	}
 
 	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
