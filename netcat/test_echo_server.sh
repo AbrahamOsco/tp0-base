544ea97f8b852a3278e53ce35c6109d6872bbdfb
Abraham Osco <aosco@fi.uba.ar>
2024-09-02 17:38:39 -0300

    add timeout in nc

--- netcat/test_echo_server.sh
+++ netcat/test_echo_server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 test_words="hello world"
-response=$(echo "$test_words" | nc -v server 12345)
+response=$(echo "$test_words" | nc -v -w 5 -W 5 server 12345)
 if [ "$test_words" = "$response" ]; then
     echo "action: test_echo_server | result: success"
 else 
