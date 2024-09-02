d9a878198c45310fd2c75881374005d4a7b625ea
Abraham Osco <aosco@fi.uba.ar>
2024-09-02 13:56:17 -0300

    now show output up too

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,7 +1,8 @@
 name_image="netcat:v1"
 docker build -f ./netcat/Dockerfile -t "$name_image" . 
 # Capturamos toda la salida al hacer up del container, obtendremos hasta el resultado de ejecutar el script adentro del container 
-up_output=$(docker compose -f ./netcat/docker-compose-netcat.yaml up) 
+up_output=$(docker compose -f ./netcat/docker-compose-netcat.yaml up )
+echo "$up_output"
 docker compose -f ./netcat/docker-compose-netcat.yaml stop
 docker compose -f ./netcat/docker-compose-netcat.yaml down
 echo "$up_output" | grep -oP 'action: test_echo_server \| result: (success|fail)'
